b938d8ce24a264c44e7f48e2e3c314e5e0345689
Gustavo Andrade <gusta.andradev@gmail.com>
2022-11-12 16:49:44 -0300

    models and repository

---
+++ authuser/src/main/java/com/ead/authuser/enums/UserStatus.java
@@ -0,0 +1,6 @@
+package com.ead.authuser.enums;
+
+public enum UserStatus {
+    ACTIVE,
+    BLOCKED,
+}
---
+++ authuser/src/main/java/com/ead/authuser/enums/UserType.java
@@ -0,0 +1,7 @@
+package com.ead.authuser.enums;
+
+public enum UserType {
+    ADMIN,
+    STUDENT,
+    INSTRUCTOR
+}
