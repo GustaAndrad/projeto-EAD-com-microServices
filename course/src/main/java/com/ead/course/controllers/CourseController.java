fb0644798b477ae8cc6e53191c1e444350515646
Gustavo Andrade <gusta.andradev@gmail.com>
2023-03-01 22:29:46 -0300

    Custom Validation

--- course/src/main/java/com/ead/course/controllers/CourseController.java
+++ course/src/main/java/com/ead/course/controllers/CourseController.java
@@ -4,6 +4,7 @@ import com.ead.course.dtos.CourseDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.specifications.SpecificationTemplate;
+import com.ead.course.validation.CourseValidator;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -30,10 +32,17 @@ public class CourseController {
     @Autowired
     CourseService courseService;
 
+    @Autowired
+    CourseValidator courseValidator;
+
 
     @PostMapping
-    public ResponseEntity<Object> saveCourse(@RequestBody @Valid CourseDto courseDto){
+    public ResponseEntity<Object> saveCourse(@RequestBody CourseDto courseDto, Errors errors){
         log.debug("POST saveCourse courseDto received {} ", courseDto.toString());
+        courseValidator.validate(courseDto, errors);
+        if(errors.hasErrors()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.getAllErrors());
+        }
         var courseModel = new CourseModel();
         BeanUtils.copyProperties(courseDto, courseModel);
         courseModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
---
+++ course/src/main/java/com/ead/course/validation/CourseValidator.java
@@ -0,0 +1,57 @@
+package com.ead.course.validation;
+
+import com.ead.course.clients.AuthUserClient;
+import com.ead.course.dtos.CourseDto;
+import com.ead.course.dtos.UserDto;
+import com.ead.course.enums.UserType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.web.client.HttpStatusCodeException;
+
+import java.util.UUID;
+
+@Component
+public class CourseValidator implements Validator {
+
+    @Autowired
+    @Qualifier("defaultValidator")
+    private Validator validator;
+
+    @Autowired
+    AuthUserClient authUserClient;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        CourseDto courseDto = (CourseDto) o;
+        validator.validate(courseDto, errors);
+        if(!errors.hasErrors()){
+            validateUserInstructor(courseDto.getUserInstructor(), errors);
+        }
+    }
+
+    private void validateUserInstructor(UUID userInstructor, Errors errors){
+        ResponseEntity<UserDto> responseUserInstructor;
+        try {
+            responseUserInstructor = authUserClient.getOneUserById(userInstructor);
+            if(responseUserInstructor.getBody().getUserType().equals(UserType.STUDENT)){
+                errors.rejectValue("userInstructor", "UserInstructorError", "User must be Instructor or Admin");
+            }
+        }catch (HttpStatusCodeException e){
+            if(e.getStatusCode().equals(HttpStatus.NOT_FOUND)){
+                errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found");
+            }
+        }
+
+
+    }
+}
