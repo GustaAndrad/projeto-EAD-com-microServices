e0a1d0a2e03bc3ef0ac7e5b1e9a3eaa7a63efd26
Gustavo Andrade <gusta.andradev@gmail.com>
2023-03-04 22:27:28 -0300

    Client-side Discovery Pattern

--- course/src/main/java/com/ead/course/configs/RestTemplateConfig.java
+++ course/src/main/java/com/ead/course/configs/RestTemplateConfig.java
@@ -1,6 +1,7 @@
 package com.ead.course.configs;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder){
         return builder.build();
