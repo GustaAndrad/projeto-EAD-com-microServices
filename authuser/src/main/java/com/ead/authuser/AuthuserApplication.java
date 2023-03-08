438463aac53b577a2a221ec9cc10d841d4d3f050
Gustavo Andrade <gusta.andradev@gmail.com>
2023-03-07 22:22:29 -0300

    Eureka server registry

--- authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
+++ authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -2,11 +2,11 @@ package com.ead.authuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class AuthuserApplication {
 
 	public static void main(String[] args) {
