8e53337a6189cb8002ad60953055d53026d5f2b6
Gustavo Da Silva Andrade <Gustavo.Andrade@UNION.corp>
2023-08-23 10:40:56 -0300

    exemplo da aplicação de retry do resilience4j

--- authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,10 +3,12 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -27,22 +30,30 @@ public class CourseClient {
     @Autowired
     UtilsService utilsService;
 
+    //Exemplo de uso do @Retry ( foi comentado pois sera usado circuit break )
+    //    @Retry(name = "retryInstance", fallbackMethod = "retryfallback")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
-        List<CourseDto> searchResult;
-        ResponseEntity<ResponsePageDto<CourseDto>> result = null;
+        List<CourseDto> searchResult = null;
         String url = utilsService.createUrlGetAllCoursesByUser(userId, pageable);
         log.debug("Request URL: {}", url);
         log.info("Request URL: {}", url);
         try {
-            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {
-            };
-            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+            ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
             searchResult = result.getBody().getContent();
             log.debug("Response Number of Elements: {}", searchResult.size());
         } catch (HttpStatusCodeException e) {
             log.error("Error request /courses {}", e);
         }
         log.info("Ending request /courses userId {}", userId);
-        return result.getBody();
+        assert searchResult != null;
+        return new PageImpl<>(searchResult);
+    }
+
+    //Exemplo de fallback quando se usa retry
+    public Page<CourseDto> retryfallback (UUID userId, Pageable pageable, Throwable t){
+        log.error("Inside retry retryfallback, cause - {}", t.toString());
+        List<CourseDto> searchResult = new ArrayList<>();
+        return new PageImpl<>(searchResult);
     }
 }
--- authuser/src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
@@ -6,12 +6,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestTemplateConfig {
+
+    static final int TIMEOUT = 5000;
 
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        return builder
+                .setConnectTimeout(Duration.ofMillis(TIMEOUT))
+                .setReadTimeout(Duration.ofMillis(TIMEOUT))
+                .build();
     }
 }
