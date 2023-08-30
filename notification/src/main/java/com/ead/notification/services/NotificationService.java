44856352b126298405edb306e47346187d03aed9
Gustavo Da Silva Andrade <Gustavo.Andrade@UNION.corp>
2023-08-30 15:26:35 -0300

    Criando NotificationConsumer
    criando exchange topic e definindo queue e bindings

--- notification/src/main/java/com/ead/notification/services/NotificationService.java
+++ notification/src/main/java/com/ead/notification/services/NotificationService.java
@@ -1,4 +1,7 @@
 package com.ead.notification.services;
 
+import com.ead.notification.models.NotificationModel;
+
 public interface NotificationService {
+    NotificationModel SaveNotification(NotificationModel notificationModel);
 }
--- notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
+++ notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -1,14 +1,21 @@
 package com.ead.notification.services.impl;
 
+import com.ead.notification.models.NotificationModel;
+import com.ead.notification.repositories.NotificationRepository;
 import com.ead.notification.services.NotificationService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
 
-    final NotificationService notificationService;
+    final NotificationRepository notificationRepository;
 
-    public NotificationServiceImpl(NotificationService notificationService) {
-        this.notificationService = notificationService;
+    public NotificationServiceImpl(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    @Override
+    public NotificationModel SaveNotification(NotificationModel notificationModel) {
+        return notificationRepository.save(notificationModel);
     }
 }
